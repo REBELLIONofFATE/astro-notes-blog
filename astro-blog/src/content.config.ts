5fd28925815ea7edec85629ffcd20a7a0f865f0d
RebellionOfFate <3200525742@qq.com>
2026-06-11 09:34:43 +0800

    refactor: 将 astro-loader-external 内联至 astro-blog/src/loaders/external
    - 移动 loader/asset-copier/types/index 四个源文件至 astro-blog/src/loaders/external/
    - 更新 content.config.ts import 路径为相对引用
    - 移除 package.json 中 astro-loader-external 的 file: 依赖
    - 删除 astro-loader-external/ 目录
    - 清理 .gitignore 中相关忽略规则

--- astro-blog/src/content.config.ts
+++ astro-blog/src/content.config.ts
@@ -1,7 +1,7 @@
 import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
-import { notesLoader } from 'astro-loader-external';
+import { notesLoader } from './loaders/external';
 import { contentSchema } from './frontmatter-schema';
 import { ROUTE_PREFIX, ASSETS_PREFIX } from './config';
 
